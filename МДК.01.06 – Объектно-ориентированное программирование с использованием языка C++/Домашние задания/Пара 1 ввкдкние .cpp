5142634bc56d80da20201e377933188b2253797d
facetar825 <facetar825@gmail.com>
2026-04-21 22:02:44 +0800

    DZ delai

---
+++ МДК.01.06 – Объектно-ориентированное программирование с использованием языка C++/Домашние задания/Пара 1 ввкдкние .cpp
@@ -0,0 +1,34 @@
+﻿
+
+#include <iostream>
+
+using namespace std;
+class Car {
+private:
+	string model = "Audi";
+public:
+	int fuel = 20;
+	int drive(int distance) {
+		fuel -= (distance / 100);
+		return fuel;
+	};
+	int refuel(int amount) {
+		fuel += amount;
+		return fuel;
+	};
+	int getFuel() {
+		cout << fuel << endl;
+		return fuel;
+	};
+
+};
+
+int main() {
+	Car car;
+	car.drive(1000);
+	car.getFuel();
+	car.refuel(5);
+	car.getFuel();
+
+
+}
