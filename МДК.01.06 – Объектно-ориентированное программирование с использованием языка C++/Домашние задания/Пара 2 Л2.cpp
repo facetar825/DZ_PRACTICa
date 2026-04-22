8434f1108f3e2e738be77877e4cd2bbe2a04676e
facetar825 <facetar825@gmail.com>
2026-04-22 09:00:09 +0800

    дз далай

---
+++ МДК.01.06 – Объектно-ориентированное программирование с использованием языка C++/Домашние задания/Пара 2 Л2.cpp
@@ -0,0 +1,47 @@
+﻿
+#include <iostream>
+#include <locale>
+#include <string>
+using namespace std;
+class Book {
+private:
+    int stran;
+    string name;
+public:
+    Book() {
+        stran = 0;
+        name = "Книга";
+    }
+    Book(string bookName, int bookStran) {
+        name = bookName;
+        stran = bookStran;
+    }
+    int getSran()const {
+        return stran;
+    }
+    void show_info() {
+        cout << "Название: " << name << endl;
+        cout << "Страниц: " << stran << endl;
+    }
+
+
+};
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    Book book1;
+    cout << "Книга 1:" << endl;
+    book1.show_info();
+
+    cout << "\n";
+
+  
+    Book book2("Война и мир", 1225);
+    cout << "Книга 2:" << endl;
+    book2.show_info();
+
+    return 0;
+    
+}
+
