5bdbc733f9e24ea28573ed170e99554a51a71297
facetar825 <facetar825@gmail.com>
2026-04-22 10:15:48 +0800

    дз делаю 3 лекция

---
+++ МДК.01.06 – Объектно-ориентированное программирование с использованием языка C++/Домашние задания/Пара 3 Л3.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+
+class String {
+private:
+    char* data;
+    size_t length;
+public:
+    String(const char* str = "") {
+        length = strlen(str);
+        data = new char[length + 1];
+        strcpy(data, str);
+    }
+
+    String(const String& other) {
+        length = other.length;
+        data = new char[length + 1];
+        strcpy(data, other.data);
+    }
+
+    String& operator=(const String& other) {
+        if (this != &other) {
+            delete[] data;
+            length = other.length;
+            data = new char[length + 1];
+            strcpy(data, other.data);
+        }
+        return *this;
+    }
+
+    ~String() {
+        delete[] data;
+    }
+
+    void print() const {
+        cout << data << endl;
+    }
+};
