64670dc3bf07c2939ac2c084d6204b1d3893b2a1
facetar825 <facetar825@gmail.com>
2026-04-24 12:33:27 +0800

    9 пара

---
+++ МДК.01.06 – Объектно-ориентированное программирование с использованием языка C++/Домашние задания/Лекция 6.cpp
@@ -0,0 +1,149 @@
+﻿
+#include <iostream>
+using namespace std;
+class Person {
+private:
+    int age;
+
+public:
+    void setAge(int a) {
+        if (a >= 0 && a <= 120) {
+            age = a;
+        }
+    };
+    int getAge() {
+        return age;
+    };
+};
+class StudentP {
+private:
+    string name;
+public:
+
+    void setName(string name) {
+        this->name = name;
+
+    };
+};
+class Product {
+private:
+    string name;
+    double price;
+
+public:
+
+    void setName(string name) {
+        this->name = name;
+    };
+
+    void setPrice(double price) {
+        if (price > 0) {
+            this->price = price;
+        }
+    };
+    string getName() {
+        return name;
+    };
+    double getPrice() {
+        return price;
+    };
+
+};
+
+class Clothes {
+public:
+    enum class TypeClothes { TSHIRT, JEANS, COAT, SUIT, SHORTS };
+
+private:
+    //Я тут плохо понимать  что на было сделать и я поросил ии надеюсь правильно с эти тип одежды
+    
+    TypeClothes typeClothes;
+    string name;
+    double price;
+    int size;
+    string color;
+public:
+    void setName(string name) {
+        this->name = name;
+    };
+    void setPrice(double price) {
+        if (price > 0) {
+            this->price = price;
+        };
+    };
+    void setType(TypeClothes newType) {
+       typeClothes = newType;
+    };
+    void setCategory(string newCategory) {
+        // При установке категории нужно выбрать подходящий тип одежды
+        if (newCategory == "Повседневная") {
+            typeClothes = TypeClothes::TSHIRT;  // или JEANS
+        }
+        else if (newCategory == "Верхняя одежда") {
+            typeClothes = TypeClothes::COAT;
+        }
+        else if (newCategory == "Деловая") {
+            typeClothes = TypeClothes::SUIT;
+        }
+        else if (newCategory == "Спортивная") {
+            typeClothes = TypeClothes::SHORTS;
+        }
+    };
+    void setSize(int size) {
+        this->size = size;
+    };
+    void setColor(string color) {
+        this->color = color;
+    };
+    string getCategory() {
+        // Вычисляем категорию на основе typeClothes
+        switch (typeClothes) {
+        case TypeClothes::TSHIRT:
+        case TypeClothes::JEANS:
+            return "Повседневная";
+        case TypeClothes::COAT:
+            return "Верхняя одежда";
+        case TypeClothes::SUIT:
+            return "Деловая";
+        case TypeClothes::SHORTS:
+            return "Спортивная";
+        default:
+            return "Не определена";
+        }
+    };
+    string getName() {
+        return name;
+    };
+    double getPrice() {
+        return price;
+    };
+    int getSize() {
+        return size;
+    };
+    string getColor() {
+        return color;
+    }
+
+
+};
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    Clothes item;
+
+    item.setName("Футболка");
+    item.setPrice(1500);
+    item.setType(Clothes::TypeClothes::TSHIRT);
+    item.setSize(42);
+    item.setColor("Красный");
+
+    cout << item.getName() << endl;      
+    cout << item.getPrice() << endl;    
+    cout << item.getCategory() << endl; 
+
+    return 0;
+
+
+}
+
---
+++ МДК.01.06 – Объектно-ориентированное программирование с использованием языка C++/Домашние задания/Лекция 7.cpp
@@ -0,0 +1,132 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+class Student {
+private:
+    static int count;
+    
+public:
+    
+
+    Student() {
+        count++;
+    };
+
+    static int getCount() {
+        return count;
+    };
+
+};
+class MathUtils {
+
+public:
+    static int add(int a, int b) {
+        return a + b;
+    };
+    // Я НЕ ЗНАЮ ЧТО ТАКОЕ САБ ДУМАЮ ЭТО ДЕЛЕНИЕ 
+    static int sub(int a, int b) {
+        return a / b;
+    };
+    //я не особо понял зачем статик 
+    static int mul(int a, int b) {
+        return a * b;
+    };
+};
+class Test {
+private:
+    int value;
+public:
+
+    static void print() {
+
+    };
+};
+class Counter {
+    
+private:
+    static int count;
+public:
+
+    Counter() {
+        count++;
+    };
+
+    static int getCount() {
+        return count;
+    };
+
+};
+class Product {
+private:
+    // а зачем цена и имя
+    string name;
+    double price;
+    static int productCount;
+
+public:
+    void setName(string name) {
+        this->name = name;
+    };
+    void setPrice(double price) {
+        this->price = price;
+    };
+
+    Product() {
+        productCount++;
+    };
+    static int getCount() {
+        return productCount;
+    };
+    string getName() {
+        return name;
+    };
+
+    double getPrice() {
+        return price;
+    };
+};
+int Product::productCount = 0;
+int Counter::count = 0;
+int Student::count = 0;
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    // прикол я не знал если без скобок написать то адрес памяти показывает минут 5 думал что не так 
+    cout << Student::getCount() << endl;
+    Student s1;
+    Student s2;
+    Student s3;
+    cout <<"Количество обьектов"<< Student::getCount() << endl;
+   
+
+    MathUtils M1;
+    int result = MathUtils::add(5, 3);
+    cout <<"Слажение "<< M1.add(2, 5) << endl;
+    cout <<"Умножение "<< M1.mul(2, 10) << endl;
+    cout <<"Деление " << M1.sub(10, 2) << endl;
+    //саб наверно сабдивижон в бледере это деление
+
+    Counter a;
+    Counter b;
+    Counter c;
+
+    cout << a.getCount() << endl;
+    cout << Counter::getCount() << endl;
+
+
+    Product p1;
+    Product p2;
+    Product p3;
+    cout << "Количетсво продукта " << Product::getCount() << endl;
+    p1.setName("Кукр");
+    p1.setPrice(200.12);
+    cout << p1.getName() << endl;
+    cout << p1.getPrice() << endl;
+
+
+
+
+}
+
