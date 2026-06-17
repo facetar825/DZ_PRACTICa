2b62d827c5bf823c2922de7531a69f7a524b2f42
BasedTree811 <facetar825@gmail.com>
2026-06-18 03:43:08 +0800

    30.03

---
+++ МДК.01.09 – Основы разработки приложений с использованием Windows Forms и WPF/Домашние задания/HomwWork 30_03/HomwWork 30_03/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace HomwWork_30_03
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            button1 = new Button();
+            richTextBox1 = new RichTextBox();
+            label1 = new Label();
+            richTextBox2 = new RichTextBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(583, 24);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(181, 430);
+            button1.Name = "button1";
+            button1.Size = new Size(196, 36);
+            button1.TabIndex = 2;
+            button1.Text = "Заказать ";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(181, 352);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(207, 32);
+            richTextBox1.TabIndex = 3;
+            richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(179, 332);
+            label1.Name = "label1";
+            label1.Size = new Size(31, 15);
+            label1.TabIndex = 4;
+            label1.Text = "куда";
+            label1.Click += label1_Click;
+            // 
+            // richTextBox2
+            // 
+            richTextBox2.Location = new Point(179, 295);
+            richTextBox2.Name = "richTextBox2";
+            richTextBox2.Size = new Size(209, 34);
+            richTextBox2.TabIndex = 5;
+            richTextBox2.Text = "";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(181, 275);
+            label2.Name = "label2";
+            label2.Size = new Size(46, 15);
+            label2.TabIndex = 6;
+            label2.Text = "от куда";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(181, 44);
+            label3.Name = "label3";
+            label3.Size = new Size(35, 15);
+            label3.TabIndex = 7;
+            label3.Text = "Цена";
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(242, 44);
+            label4.Name = "label4";
+            label4.Size = new Size(13, 15);
+            label4.TabIndex = 8;
+            label4.Text = "0";
+            label4.Click += label4_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LavenderBlush;
+            ClientSize = new Size(583, 634);
+            Controls.Add(label3);
+            Controls.Add(label4);
+            Controls.Add(label2);
+            Controls.Add(richTextBox2);
+            Controls.Add(label1);
+            Controls.Add(richTextBox1);
+            Controls.Add(button1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private MenuStrip menuStrip1;
+        private Button button1;
+        private RichTextBox richTextBox1;
+        private Label label1;
+        private RichTextBox richTextBox2;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+    }
+}
---
+++ МДК.01.09 – Основы разработки приложений с использованием Windows Forms и WPF/Домашние задания/HomwWork 30_03/HomwWork 30_03/Form1.cs
@@ -0,0 +1,31 @@
+namespace HomwWork_30_03
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.BackColor = Color.Yellow;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label4.Text = "500 рублей";
+        }
+    }
+}
